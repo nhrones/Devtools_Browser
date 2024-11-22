bb8189259b1a63e813db64f90a65c7b516b61b40
NDH <ndhrones@gmail.com>
2024-11-22 17:19:45 -0500

    first commit

---
+++ browser.ts
@@ -0,0 +1,31 @@
+/**
+ * Get the browser open command based on the os
+ * @returns the string for the command to call
+ */
+async function getBrowserCmd(): Promise<string> {
+   switch (Deno.build.os) {
+      case "windows":
+         return "explorer.exe";
+      case "darwin":
+         return "open";
+      case "linux":
+         if ((await Deno.permissions.query({ name: "env" })) && Deno.env.get("WSL_DISTRO_NAME")) {
+            // is WSL
+            return "explorer.exe";
+         } else {
+            return "xdg-open";
+         }
+      default:
+         return "Unknown os" 
+    }
+}
+
+/**
+ * Opens a website in the default browser
+ * @param url  - the url to be opened in the browser
+ * @example await openWebsite('https://Deno.com')
+ */
+export async function openWebsite(url: string) {
+   return new Deno.Command(await getBrowserCmd(), 
+   { args: [url] }).outputSync();
+}
